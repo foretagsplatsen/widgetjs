dd0f8cd88a3f32f22f223f2af7ba1a85891a1530
Damien Cassou <damien@cassou.me>
2019-07-08 14:46:01 +0200

    WIP

--- src/widgetjs.js
+++ src/widgetjs.js
@@ -1,13 +1,15 @@
 define([
 	"./htmlCanvas",
 	"./widget",
+	"./widget2",
 	"./widget-extensions",
 	"./router",
 	"./events"
-], function(htmlCanvas, widget, widgetExtensions, router, events) {
+], function(htmlCanvas, widget, widget2, widgetExtensions, router, events) {
 	return {
 		htmlCanvas: htmlCanvas,
 		widget: widget,
+		Widget: widget2,
 		ext: widgetExtensions,
 		router: router,
 		events: events
