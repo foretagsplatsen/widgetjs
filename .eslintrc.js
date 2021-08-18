4d9c760710d683855303cac5469c9ef54abefbac
Damien Cassou <damien@cassou.me>
2021-08-18 17:48:44 +0200

    Add import rules to eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@
 
 	module.exports = {
 		root: true,
-		extends: "ftgp",
+		extends: ["ftgp", "plugin:import/recommended"],
 		parserOptions: {
 			ecmaVersion: 6,
 			sourceType: "module"
@@ -11,7 +11,11 @@
 		rules: {
 			"no-console": [error],
 			"quotes": [error, "double"],
-			"ftgp/require-class-comment": 0
+			"ftgp/require-class-comment": 0,
+			"import/no-absolute-path": "error",
+			"import/no-self-import": "error",
+			"import/no-useless-path-segments": "error",
+			"import/no-cycle": "error",
 		}
 	};
 })();
