416446dc2126b7a7f5aaf3af7438add831081f9b
Damien Cassou <damien@cassou.me>
2023-07-13 15:41:48 +0200

    33476855: Get rid of an unnecessary IIFE in .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,17 @@
-(function() {
-	var error = 2;
-
-	module.exports = {
-		root: true,
-		extends: ["plugin:@foretagsplatsen/eslint-plugin-ftgp/main", "plugin:import/recommended"],
-		parserOptions: {
-			ecmaVersion: 6,
-			sourceType: "module"
-		},
-		rules: {
-			"no-console": [error],
-			"quotes": [error, "double"],
-			"ftgp/require-class-comment": 0,
-			"import/no-absolute-path": "error",
-			"import/no-self-import": "error",
-			"import/no-useless-path-segments": "error",
-			"import/no-cycle": "error",
-		}
-	};
-})();
+module.exports = {
+	root: true,
+	extends: ["plugin:@foretagsplatsen/eslint-plugin-ftgp/main", "plugin:import/recommended"],
+	parserOptions: {
+		ecmaVersion: 6,
+		sourceType: "module"
+	},
+	rules: {
+		"no-console": ["error"],
+		"quotes": ["error", "double"],
+		"ftgp/require-class-comment": 0,
+		"import/no-absolute-path": "error",
+		"import/no-self-import": "error",
+		"import/no-useless-path-segments": "error",
+		"import/no-cycle": "error",
+	}
+}
