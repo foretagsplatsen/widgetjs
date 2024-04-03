c38b4d97efdf3f62d70143292dfc61ca065a46c3
Damien Cassou <damien@cassou.me>
2024-04-03 10:39:48 +0200

    32129139: Get rid of webpack

--- webpack.config.js
+++
@@ -1,6 +0,0 @@
-const config = require("./webpack.base.config.js");
-
-config.entry = "./src/widgetjs.js";
-config.output.filename = "widgetjs.js";
-
-module.exports = config;
--- webpack.jasmine.config.js
+++
@@ -1,6 +0,0 @@
-const config = require("./webpack.base.config.js");
-
-config.entry = "./src/test/tests.js";
-config.output.filename = "jasmine.js";
-
-module.exports = config;
