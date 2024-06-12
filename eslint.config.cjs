cc5fd4b1e230e4ab94a830ff1d7cc9196ff23d5c
Damien Cassou <damien@cassou.me>
2024-06-12 16:07:10 +0200

    37849678: Bump @foretagsplatsen/eslint-plugin

---
+++ eslint.config.cjs
@@ -0,0 +1,62 @@
+const globals = require("globals");
+
+const finsitPlugin = require("@foretagsplatsen/eslint-plugin");
+
+module.exports = [
+	{
+		name: "global ignores",
+		ignores: ["dist/", "coverage/", "sample/"],
+	},
+	...finsitPlugin.configs.main,
+	{
+		name: "globals",
+		languageOptions: {
+			globals: {
+				...globals.browser,
+			},
+		},
+	},
+	{
+		name: "global rules",
+		rules: {
+			"import/no-unused-modules": [
+				"error",
+				{
+					unusedExports: true,
+					missingExports: true,
+					ignoreExports: [
+						// List of files exporting stuff which are not imported:
+						"src/widgetjs.js",
+						"vitest.config.js",
+						"eslint.config.cjs",
+						".eslintrc.cjs",
+						// List of files not exporting anything:
+						"src/router/optionalParameterSegment.js",
+						"src/router/staticSegment.js",
+					],
+				},
+			],
+		},
+	},
+	{
+		name: "test rules",
+		files: ["src/test/**/*.js"],
+		rules: {
+			"import/no-unused-modules": [
+				"error",
+				{
+					unusedExports: true,
+					missingExports: true,
+					// List of files not exporting anything:
+					ignoreExports: ["**/*Test.js"],
+				},
+			],
+		},
+	},
+	{
+		name: "Disable import/no-unused-modules as it's flaky AF",
+		rules: {
+			"import/no-unused-modules": "off",
+		},
+	},
+];
