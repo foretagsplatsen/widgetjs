db45620d1ee405dc5e147e89ba6ab71161a284bf
Damien Cassou <damien@cassou.me>
2024-06-12 16:07:10 +0200

    37849678: Bump @foretagsplatsen/eslint-plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,37 +1,4 @@
-/* eslint-env commonjs */
-
+// This is needed for eslint-plugin-import
 module.exports = {
 	root: true,
-	extends: ["plugin:@foretagsplatsen/main"],
-	// workaround for plugin:@foretagsplatsen/main enabling many
-	// unwanted environments:
-	env: {
-		browser: true,
-		es2024: true,
-	},
-	ignorePatterns: ["sample", "coverage"],
-	rules: {
-		"import/no-unused-modules": [
-			"error",
-			{
-				unusedExports: true,
-				missingExports: true,
-				ignoreExports: [
-					// List of files exporting stuff which are not imported:
-					"src/widgetjs.js",
-					"./vitest.config.js",
-					// List of files not exporting anything:
-					"**/.eslintrc.cjs",
-					"src/router/optionalParameterSegment.js",
-					"src/router/staticSegment.js",
-				],
-			},
-		],
-	},
-	settings: {
-		"import/resolver": {
-			exports: {},
-			node: true,
-		},
-	},
 };
