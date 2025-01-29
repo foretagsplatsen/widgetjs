576be5f82ef8f364e831e73a508b42665c573713
Damien Cassou <damien.cassou@wolterskluwer.com>
2025-01-29 16:35:32 +0100

    40698660: Add initial tests to Widget2.isRendered()

---
+++ src/test/Widget2Test.js
@@ -0,0 +1,43 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import htmlCanvas from "../htmlCanvas.js";
+import jQuery from "jquery";
+import Widget2 from "../Widget2.js";
+
+describe("Widget", () => {
+	beforeEach(() => {
+		document.body.replaceChildren();
+	});
+
+	describe("isRendered()", () => {
+		it("returns false if the widget isn't attached to the DOM", () => {
+			const widget = makeWidget();
+
+			expect(widget.isRendered()).toBeFalsy();
+		});
+
+		it("returns true if the widget is attached to the DOM", () => {
+			const widget = makeWidget();
+
+			makeHtml().render(widget);
+
+			expect(widget.isRendered()).toBeTruthy();
+		});
+	});
+});
+
+function makeWidget() {
+	class Foo extends Widget2 {
+		renderContentOn(html) {
+			html.p("content");
+		}
+	}
+
+	return new Foo();
+}
+
+function makeHtml() {
+	jQuery("BODY").append('<div id="sandbox"></div>');
+	const sandbox = jQuery("#sandbox");
+
+	return htmlCanvas(sandbox);
+}
